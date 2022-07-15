76bd7d996e3211827039346666dd1459546b2158
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-07-15 14:55:25 +0200

    delete blob storage service

--- engine/services/data_service_factory.py
+++ engine/services/data_service_factory.py
@@ -1,4 +1,3 @@
-from engine.services.blob_data_service import BlobDataService
 from engine.services.local_data_service import LocalDataService
 from engine.dummy_services.dummy_data_service import DummyDataService
 
@@ -9,19 +8,8 @@ class DataServiceFactory:
         self.cache_service = cache_service
 
     def get_data_service(self):
-        data_service_type = self.config.getValue("DATA_SERVICE_TYPE")
-        if data_service_type == "azure_blob":
-            return BlobDataService.get_instance(
-                blob_storage_connection_string=self.config.getValue(
-                    "BLOB_STORAGE_CONNECTION_STRING"
-                ),
-                container=self.config.getValue("CONTAINER"),
-                cache_service=self.cache_service,
-            )
-        else:
-            return LocalDataService.get_instance(
-                cache_service=self.cache_service
-            )
+        # TODO extend the method as more services are available
+        return LocalDataService.get_instance(cache_service=self.cache_service)
 
     def get_dummy_data_service(self, data):
         return DummyDataService(data)
