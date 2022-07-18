2fc0e5a787265c042ecb4f4f593c47609d72dd92
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-07-18 16:13:57 +0200

    add "black" formatter

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # cdisc-rules-engine
 Open source offering of the cdisc rules engine
 
+### Code formatter
+This project uses the `black` code formatter for python.
+It also uses `pre-commit` to run `black` when you commit.
+Both dependencies are added to *requirements.txt*.
+
+**Required**
+
+Setting up `pre-commit` requires one extra step. After installing it you have to run 
+
+`pre-commit install`
+
+This installs `pre-commit` in your `.git/hooks` directory.
+
 ### Running The Tests
 From the root of the project run the following command:
 
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 import setuptools
 
 setuptools.setup(
-        name='cdisc-rules-engine',
-        version="0.1.0",
-        description="Open source offering of the cdisc rules engine",
-        author='cdisc-org',
-        url='https://github.com/cdisc-org/cdisc-rules-engine',
-        packages=['engine'],
-        license='MIT'
+    name="cdisc-rules-engine",
+    version="0.1.0",
+    description="Open source offering of the cdisc rules engine",
+    author="cdisc-org",
+    url="https://github.com/cdisc-org/cdisc-rules-engine",
+    packages=["engine"],
+    license="MIT",
 )
