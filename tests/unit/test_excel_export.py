15c9fea16896ee26edc104afb651a05757fb0242
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-08-12 13:08:01 +0200

    fix unit test

--- tests/unit/test_excel_export.py
+++ tests/unit/test_excel_export.py
@@ -4,9 +4,7 @@ from typing import List
 from cdisc_rules_engine.models.rule_validation_result import RuleValidationResult
 from cdisc_rules_engine.utilities.excel_report import ExcelReport
 
-test_report_template: str = (
-    f"{os.path.dirname(__file__)}/../../resources/templates/report-template.xlsx"
-)
+test_report_template: str = f"{os.path.dirname(__file__)}/../../cdisc_rules_engine/resources/templates/report-template.xlsx"
 
 mock_validation_results = [
     RuleValidationResult(
