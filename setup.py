736c09030c0daf8854aff76ca07fd4dccd8613c0
Sitiaev Denis <denis_sitiaev@epam.com>
2022-08-18 14:29:12 +0200

    add flake8 and provide simple config

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
     include_package_data=True,
     python_requires=">=3.9",
     install_requires=[
-        "pytest==7.1.2",
         "pandas==1.3.5",
         "business-rules-enhanced==1.2.1",
         "python-dotenv==0.20.0",
@@ -22,10 +21,7 @@ setuptools.setup(
         "odmlib==0.1.4",
         "xport==3.6.1",
         "redis==4.0.2",
-        "black==22.6.0",
-        "pre-commit==2.20.0",
         "openpyxl==3.0.10",
-        "click==8.1.3",
         "pyinstaller==5.2",
         "click==8.1.3",
     ],
