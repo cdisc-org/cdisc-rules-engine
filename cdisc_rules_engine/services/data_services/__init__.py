fd69df253ec2c31e60dfb5ea537ac4298e4e0609
Nicholas Haydel <nhaydel@cdisc.org>
2022-08-23 08:15:44 -0500

    Fix imports

--- cdisc_rules_engine/services/data_services/__init__.py
+++ cdisc_rules_engine/services/data_services/__init__.py
@@ -1,12 +1,12 @@
 from .base_data_service import BaseDataService, cached_dataset
-from .data_service_factory import DataServiceFactory
 from .local_data_service import LocalDataService
 from .dummy_data_service import DummyDataService
+from .data_service_factory import DataServiceFactory
 
 __all__ = [
     "BaseDataService",
     "cached_dataset",
     "LocalDataService",
     "DataServiceFactory",
-    "DummyDataService"
+    "DummyDataService",
 ]
--- cdisc_rules_engine/utilities/data_processor.py
+++ cdisc_rules_engine/utilities/data_processor.py
@@ -7,7 +7,6 @@ from uuid import uuid4
 import pandas as pd
 
 from cdisc_rules_engine.config import config
-from cdisc_rules_engine.services.data_services.dummy_data_service import DummyDataService
 from cdisc_rules_engine.exceptions.custom_exceptions import InvalidMatchKeyError
 from cdisc_rules_engine.models.dictionaries.meddra.meddra_variables import (
     MedDRAVariables,
@@ -23,6 +22,7 @@ from cdisc_rules_engine.services.cdisc_library_service import CDISCLibraryServic
 from cdisc_rules_engine.services.data_services import (
     BaseDataService,
     DataServiceFactory,
+    DummyDataService,
 )
 from cdisc_rules_engine.utilities.utils import (
     get_corresponding_datasets,
