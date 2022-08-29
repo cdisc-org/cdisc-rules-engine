1b7865a63cded795b30896a2d9de32b86501bf12
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-08-29 11:02:18 +0200

    change version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="cdisc-rules-engine",
-    version="0.3.3",
+    version="0.3.3.1",
     description="Open source offering of the cdisc rules engine",
     author="cdisc-org",
     url="https://github.com/cdisc-org/cdisc-rules-engine",
