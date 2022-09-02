a7f5837921292722e9e017fc33b70bdafa169b37
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-09-02 16:24:50 +0200

    make DataReaderFactory extendable

--- cdisc_rules_engine/services/data_readers/data_reader_factory.py
+++ cdisc_rules_engine/services/data_readers/data_reader_factory.py
@@ -1,7 +1,39 @@
+from typing import Type
+
+from cdisc_rules_engine.services.data_readers.data_reader_interface import (
+    DataReaderInterface,
+)
 from cdisc_rules_engine.services.data_readers.xpt_reader import XPTReader
+from cdisc_rules_engine.services.factory_interface import FactoryInterface
 
 
-class DataReaderFactory:
-    # TODO: Expand get reader function as more data formats are available.
-    def get_reader(self):
-        return XPTReader()
+class DataReaderFactory(FactoryInterface):
+    _service_map = {
+        "xpt": XPTReader,
+    }
+
+    def __init__(self):
+        self._default_service_name: str = "xpt"
+
+    @classmethod
+    def register_service(cls, name: str, service: Type[DataReaderInterface]):
+        """
+        Registers a new service in internal _service_map
+        """
+        if not name:
+            raise ValueError("Service name must not be empty!")
+        if not issubclass(service, DataReaderInterface):
+            raise TypeError("Implementation of DataReaderInterface required!")
+        cls._service_map[name] = service
+
+    def get_service(self, name: str = None, **kwargs) -> DataReaderInterface:
+        """
+        Get instance of service that matches searched implementation
+        """
+        service_name = name or self._default_service_name
+        if service_name in self._service_map:
+            return self._service_map[service_name]()
+        raise ValueError(
+            f"Service name must be in {list(self._service_map.keys())}, "
+            f"given service name is {service_name}"
+        )
