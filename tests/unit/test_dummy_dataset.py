e6b787559e765b2ea64fd490c5f19418657d7060
nhaydel <nicholas.haydel1@gmail.com>
2022-09-07 15:06:01 -0500

    Update DummyDataService to get metadata properly

--- tests/unit/test_dummy_dataset.py
+++ tests/unit/test_dummy_dataset.py
@@ -15,3 +15,32 @@ def test_invalid_dataset_data():
     ]
     with pytest.raises(InvalidDatasetFormat):
         DummyDataset(dataset_data)
+
+
+def test_valid_dataset_data():
+    dataset_data = [
+        {
+            "domain": "AE",
+            "filename": "ae.xpt",
+            "label": "Adverse Events",
+            "records": {"AESEQ": [1, 2, 3, 4]},
+        }
+    ]
+    dataset = DummyDataset(dataset_data[0])
+    assert dataset.domain == "AE"
+
+
+def test_get_dataset_metadata():
+    dataset_data = [
+        {
+            "domain": "AE",
+            "filename": "ae.xpt",
+            "label": "Adverse Events",
+            "records": {"AESEQ": [1, 2, 3, 4]},
+        }
+    ]
+    dataset = DummyDataset(dataset_data[0])
+    metadata = dataset.get_metadata()
+    assert "dataset_name" in metadata
+    assert metadata["dataset_name"] == ["AE"]
+    assert metadata["dataset_label"] == ["Adverse Events"]
