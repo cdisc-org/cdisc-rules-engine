76611acbd088427e456cbc1266e2b852c56c64db
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-09-07 18:45:13 +0200

    move condition interface

--- cdisc_rules_engine/interfaces/__init__.py
+++ cdisc_rules_engine/interfaces/__init__.py
@@ -1,4 +1,5 @@
 from .cache_service_interface import CacheServiceInterface
+from .condition_interface import ConditionInterface
 from .data_reader_interface import DataReaderInterface
 from .data_service_interface import DataServiceInterface
 from .factory_interface import FactoryInterface
@@ -8,6 +9,7 @@ from .terms_factory_interface import TermsFactoryInterface
 
 __all__ = [
     "CacheServiceInterface",
+    "ConditionInterface",
     "DataReaderInterface",
     "DataServiceInterface",
     "FactoryInterface",
