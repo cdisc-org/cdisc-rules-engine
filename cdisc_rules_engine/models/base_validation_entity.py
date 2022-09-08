1a0b471e776d137f137a5bde0f5957f54c6f76d8
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-09-08 11:45:38 +0200

    fix import issues

--- cdisc_rules_engine/models/base_validation_entity.py
+++ cdisc_rules_engine/models/base_validation_entity.py
@@ -3,6 +3,6 @@ from abc import ABC
 from cdisc_rules_engine.interfaces import RepresentationInterface
 
 
-class BaseValidationEntity(ABC, RepresentationInterface):
+class BaseValidationEntity(RepresentationInterface, ABC):
     def __init__(self):
         self.status = None
