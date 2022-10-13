e9cd92acb6e12399733c5684dde9905bd98fd07b
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-10-13 18:02:01 +0200

    exclude resources folder from pypi package

--- setup.py
+++ setup.py
@@ -17,12 +17,9 @@ setuptools.setup(
         "business-rules-enhanced==1.2.4",
         "python-dotenv==0.20.0",
         "cdisc-library-client==0.1.4",
-        "pytest-asyncio==0.18.3",
         "odmlib==0.1.4",
         "xport==3.6.1",
         "redis==4.0.2",
         "openpyxl==3.0.10",
-        "pyinstaller==5.2",
-        "click==8.1.3",
     ],
 )
