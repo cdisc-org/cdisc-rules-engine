34deb1c17dc672882b7c866fc7d5649ac9ae6a69
nhaydel <nicholas.haydel1@gmail.com>
2022-12-14 07:30:10 -0600

    Add rule test script

--- scripts/test_rule.py
+++ scripts/test_rule.py
@@ -1,12 +1,13 @@
 import itertools
 import time
+import click
+from datetime import datetime
+import json
 from functools import partial
 from multiprocessing import Pool
 from multiprocessing.managers import SyncManager
-from typing import List, Iterable, Callable
-
+from typing import List
 from cdisc_rules_engine.config import config
-from cdisc_rules_engine.enums.progress_parameter_options import ProgressParameterOptions
 from cdisc_rules_engine.models.rule_conditions import ConditionCompositeFactory
 from cdisc_rules_engine.models.rule_validation_result import RuleValidationResult
 from cdisc_rules_engine.models.validation_args import Validation_args
@@ -16,18 +17,17 @@ from cdisc_rules_engine.services.cache import (
     InMemoryCacheService,
     RedisCacheService,
 )
-from cdisc_rules_engine.services.data_services import (
-    DataServiceFactory,
-)
+from cdisc_rules_engine.services.data_services import DataServiceFactory
+from cdisc_rules_engine.dummy_models.dummy_dataset import DummyDataset
+from cdisc_rules_engine.services.reporting import BaseReport, ReportFactory
+from cdisc_rules_engine.models.test_args import TestArgs
+from cdisc_rules_engine.models.rule import Rule
+from cdisc_rules_engine.utilities.utils import generate_report_filename
 from scripts.script_utils import (
     fill_cache_with_dictionaries,
     fill_cache_with_provided_data,
     get_cache_service,
-    get_rules,
-    get_datasets,
 )
-from cdisc_rules_engine.services.reporting import BaseReport, ReportFactory
-from cdisc_rules_engine.utilities.progress_displayers import get_progress_displayer
 
 """
 Sync manager used to manage instances of the cache between processes.
@@ -40,77 +40,101 @@ class CacheManager(SyncManager):
     pass
 
 
-def validate_single_rule(cache, datasets, args: Validation_args, rule: dict = None):
+def validate_single_rule(cache, path, args, datasets, rule: dict = None):
+    set_log_level("ERROR")
     rule["conditions"] = ConditionCompositeFactory.get_condition_composite(
         rule["conditions"]
     )
-    set_log_level(args)
     # call rule engine
     engine = RulesEngine(
         cache=cache,
-        standard=args.standard,
-        standard_version=args.version.replace(".", "-"),
+        standard="sdtmig",
+        standard_version="3-4",
         ct_package=args.controlled_terminology_package,
         meddra_path=args.meddra,
         whodrug_path=args.whodrug,
     )
     results = [
-        engine.validate_single_rule(
-            rule, dataset["full_path"], datasets, dataset["domain"]
+        engine.test_validation(
+            rule, f"{path}/{dataset.filename}", datasets, dataset.domain
         )
         for dataset in datasets
     ]
     results = list(itertools.chain(*results))
-    if args.progress == ProgressParameterOptions.VERBOSE_OUTPUT.value:
-        engine_logger.log(f"{rule['core_id']} validation complete")
     return RuleValidationResult(rule, results)
 
 
-def set_log_level(args):
-    if args.log_level.lower() == "disabled":
+def set_log_level(level: str):
+    if level == "disabled":
         engine_logger.disabled = True
     else:
-        engine_logger.setLevel(args.log_level.lower())
-    if args.progress == ProgressParameterOptions.VERBOSE_OUTPUT.value:
-        engine_logger.disabled = False
-        engine_logger.setLevel("verbose")
+        engine_logger.setLevel(level)
 
 
-def run_validation(args: Validation_args):
-    set_log_level(args)
+def test(args: TestArgs):
+    set_log_level("ERROR")
     # fill cache
     CacheManager.register("RedisCacheService", RedisCacheService)
     CacheManager.register("InMemoryCacheService", InMemoryCacheService)
     manager = CacheManager()
     manager.start()
     shared_cache = get_cache_service(manager)
-    engine_logger.info(f"Populating cache, cache path: {args.cache}")
     shared_cache = fill_cache_with_provided_data(shared_cache, args)
 
     # install dictionaries if needed
     fill_cache_with_dictionaries(shared_cache, args)
-    rules = get_rules(shared_cache, args)
+    with open(args.rule, "r") as f:
+        rules = [Rule.from_cdisc_metadata(json.load(f))]
     data_service = DataServiceFactory(config, shared_cache).get_data_service()
-    datasets = get_datasets(data_service, args.dataset_paths)
-    engine_logger.info(f"Running {len(rules)} rules against {len(datasets)} datasets")
+    with open(args.dataset_path, "r") as f:
+        data_json = json.load(f)
+    datasets = [DummyDataset(data) for data in data_json.get("datasets", [])]
 
     start = time.time()
     results = []
     # run each rule in a separate process
-    with Pool(args.pool_size) as pool:
-        validation_results: Iterable[RuleValidationResult] = pool.imap_unordered(
-            partial(validate_single_rule, shared_cache, datasets, args),
-            rules,
-        )
-        progress_handler: Callable = get_progress_displayer(args)
-        results = progress_handler(rules, validation_results, results)
+    with Pool(10) as pool:
+        with click.progressbar(
+            length=len(rules),
+            fill_char=click.style("\u2588", fg="green"),
+            empty_char=click.style("-", fg="white", dim=True),
+            show_eta=False,
+        ) as bar:
+            for rule_result in pool.imap_unordered(
+                partial(
+                    validate_single_rule, shared_cache, "./scripts", args, datasets
+                ),
+                rules,
+            ):
+                results.append(rule_result)
+                bar.update(1)
 
-    # build all desired reports
     end = time.time()
     elapsed_time = end - start
+    output_file = generate_report_filename(datetime.now().isoformat())
+
+    validation_args = Validation_args(
+        None,
+        None,
+        [args.dataset_path],
+        None,
+        "resources/templates/report-template.xlsx",
+        args.standard,
+        args.version,
+        args.controlled_terminology_package,
+        output_file,
+        ["XLSX"],
+        None,
+        args.define_version,
+        args.meddra,
+        args.whodrug,
+        rules,
+        False,
+    )
     reporting_factory = ReportFactory(
-        args.dataset_paths, results, elapsed_time, args, data_service
+        [args.dataset_path], results, elapsed_time, validation_args, data_service
     )
     reporting_services: List[BaseReport] = reporting_factory.get_report_services()
     for reporting_service in reporting_services:
         reporting_service.write_report()
+    print(f"Output: {output_file}")
