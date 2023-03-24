ef1cab34c1dc769756bf4ed6dd634ec6d5ed23e1
Gerry Campion <gcampion@cdisc.org>
2023-03-24 17:36:43 -0400

    ensure dataset list is looped once

--- cdisc_rules_engine/operations/parent_library_model_column_order.py
+++ cdisc_rules_engine/operations/parent_library_model_column_order.py
@@ -2,6 +2,7 @@ from cdisc_rules_engine.operations.library_model_column_order import (
     LibraryModelColumnOrder,
 )
 from pandas import Series
+from collections import defaultdict
 
 
 class ParentLibraryModelColumnOrder(LibraryModelColumnOrder):
@@ -18,21 +19,25 @@ class ParentLibraryModelColumnOrder(LibraryModelColumnOrder):
         The lists with column names are sorted
         in accordance to "ordinal" key of library metadata.
         """
+        domain_to_datasets = self._get_domain_to_datasets()
         rdomain_names_list = {}
         return Series(
             rdomain_names_list.setdefault(
-                rdomain, self._get_parent_variable_names_list(rdomain)
+                rdomain,
+                self._get_parent_variable_names_list(domain_to_datasets, rdomain),
             )
             for rdomain in self.params.dataframe.get("RDOMAIN", [])
         )
 
-    def _get_parent_variable_names_list(self, rdomain: str):
-        parent_datasets = [
-            dataset["filename"]
-            for dataset in self.params.datasets
-            if dataset["domain"] == rdomain
-        ]
+    def _get_domain_to_datasets(self):
+        domain_to_datasets = defaultdict(list)
+        for dataset in self.params.datasets:
+            domain_to_datasets[dataset["domain"]].append(dataset)
+        return domain_to_datasets
+
+    def _get_parent_variable_names_list(self, domain_to_datasets: dict, rdomain: str):
+        parent_datasets = domain_to_datasets.get(rdomain, [])
         if len(parent_datasets) < 1:
             return []
-        parent_dataframe = self.data_service.get_dataset(parent_datasets[0])
+        parent_dataframe = self.data_service.get_dataset(parent_datasets[0]["filename"])
         return self._get_variable_names_list(rdomain, parent_dataframe)
