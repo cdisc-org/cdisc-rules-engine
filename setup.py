5d9386b13a58b2e1d27953661e590773e24dbca6
Nic <nicholas.haydel1@gmail.com>
2023-04-05 10:32:34 -0500

    Update business rules requirement (#365)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     python_requires=">=3.8",
     install_requires=[
         "pandas>=1.3.5",
-        "business-rules-enhanced==1.3.1",
+        "business-rules-enhanced==1.3.3",
         "python-dotenv==0.20.0",
         "cdisc-library-client==0.1.4",
         "odmlib==0.1.4",
