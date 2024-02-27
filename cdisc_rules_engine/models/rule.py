6031753a9a67fcebf2d943c52b9315f36c526e6d
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2024-02-27 14:50:22 +0000

    Handle mixed case Left/Right and schema updates

--- cdisc_rules_engine/models/rule.py
+++ cdisc_rules_engine/models/rule.py
@@ -163,7 +163,12 @@ class Rule:
         for data in match_key_data:
             join_data = {
                 "domain_name": data.get("Name"),
-                "match_key": data.get("Keys", []),
+                "match_key": [
+                    key
+                    if isinstance(key, str)
+                    else {k.lower(): v for k, v in key.items()}
+                    for key in data.get("Keys", [])
+                ],
                 "wildcard": data.get("Wildcard", "**"),
             }
             if data.get("Is_Relationship", False):
