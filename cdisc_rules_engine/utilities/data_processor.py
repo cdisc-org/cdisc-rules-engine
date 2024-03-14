0efac66eb88115813afcd45fc815124567617274
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2024-03-14 15:45:25 +0000

    Support non-matching Key values and Join Type in Match Datasets (#621)
    * Support non-matching Key values and Join Type
    
    * Reformat join_types.py
    
    * Change Keys to accept left/right dict instead of delimited string
    
    * New param for test_rule to confirm left/right keys and join_key
    
    * Handle mixed case Left/Right and schema updates
    
    * Replace NaN more comprehensively
    
    * Make "_merge" columns unique for joining >2 tables, add unit tests
    
    * Parameterize unit tests
    
    * Update description for test_merge_datasets_on_join_type

--- cdisc_rules_engine/utilities/data_processor.py
+++ cdisc_rules_engine/utilities/data_processor.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Set, TYPE_CHECKING
 import pandas as pd
 
 from cdisc_rules_engine.config import config
+from cdisc_rules_engine.enums.join_types import JoinTypes
 from cdisc_rules_engine.exceptions.custom_exceptions import InvalidMatchKeyError
 from cdisc_rules_engine.services.cache.cache_service_factory import CacheServiceFactory
 from cdisc_rules_engine.interfaces import (
@@ -414,14 +415,32 @@ class DataProcessor:
         left_dataset_match_keys: List[str],
         right_dataset_match_keys: List[str],
         right_dataset_domain_name: str,
+        join_type: JoinTypes,
     ) -> pd.DataFrame:
         result = pd.merge(
             left_dataset,
             right_dataset,
+            how=join_type.value,
             left_on=left_dataset_match_keys,
             right_on=right_dataset_match_keys,
             suffixes=("", f".{right_dataset_domain_name}"),
+            indicator=(
+                False
+                if join_type is JoinTypes.INNER
+                else f"_merge_{right_dataset_domain_name}"
+            ),
         )
+        if join_type is JoinTypes.LEFT:
+            if "left_only" in result[f"_merge_{right_dataset_domain_name}"].values:
+                DummyDataService._replace_nans_in_numeric_cols_with_none(result)
+                result.loc[
+                    result[f"_merge_{right_dataset_domain_name}"] == "left_only",
+                    result.columns.symmetric_difference(
+                        left_dataset.columns.union(
+                            [f"_merge_{right_dataset_domain_name}"]
+                        )
+                    ),
+                ] = None
         return result
 
     @staticmethod
