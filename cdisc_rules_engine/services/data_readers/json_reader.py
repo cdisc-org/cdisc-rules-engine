944c4ca624a000377806662a82bd12be8c0afda2
nhaydel <nicholas.haydel1@gmail.com>
2024-05-12 14:50:26 -0500

    fix json reader to parquet method

--- cdisc_rules_engine/services/data_readers/json_reader.py
+++ cdisc_rules_engine/services/data_readers/json_reader.py
@@ -14,50 +14,64 @@ import tempfile
 
 
 class DatasetJSONReader(DataReaderInterface):
-    def from_file(self, file_path):
-        # Load Dataset-JSON Schema
+    def get_schema(self) -> dict:
         with open(
             os.path.join("resources", "schema", "dataset.schema.json")
         ) as schemajson:
             schema = schemajson.read()
-        schema = json.loads(schema)
+        return json.loads(schema)
 
+    def get_data_key(self, dataset_json: dict) -> str:
+        if "clinicalData" in dataset_json:
+            return "clinicalData"
+        return "referenceData"
+
+    def read_json_file(self, file_path: str) -> dict:
         with open(file_path, "r") as file:
             datasetjson = json.load(file)
+        return datasetjson
 
+    def parse_items_data(self, dataset_json: dict, data_key: str) -> pd.DataFrame:
+        items_data = next(
+            (
+                d
+                for d in dataset_json[data_key]["itemGroupData"].values()
+                if "items" in d
+            ),
+            {},
+        )
+        return pd.DataFrame(
+            [item[1:] for item in items_data.get("itemData", [])],
+            columns=[item["name"] for item in items_data.get("items", [])[1:]],
+        )
+
+    def _raw_dataset_from_file(self, file_path) -> pd.DataFrame:
+        schema = self.get_schema()
+        datasetjson = self.read_json_file(file_path)
+
+        jsonschema.validate(datasetjson, schema)
+        data_key = self.get_data_key(datasetjson)
+        df = self.parse_items_data(datasetjson, data_key)
+        return df.applymap(lambda x: round(x, 15) if isinstance(x, float) else x)
+
+    def from_file(self, file_path):
+        # Load Dataset-JSON Schema
         try:
-            jsonschema.validate(datasetjson, schema)
-            if "clinicalData" in datasetjson:
-                data_key = "clinicalData"
-            elif "referenceData" in datasetjson:
-                data_key = "referenceData"
-
-            items_data = next(
-                (
-                    d
-                    for d in datasetjson[data_key]["itemGroupData"].values()
-                    if "items" in d
-                ),
-                {},
-            )
-            df = pd.DataFrame(
-                [item[1:] for item in items_data.get("itemData", [])],
-                columns=[item["name"] for item in items_data.get("items", [])[1:]],
-            )
-
-            df = df.applymap(lambda x: round(x, 15) if isinstance(x, float) else x)
-
+            df = self._raw_dataset_from_file(file_path)
             if self.dataset_implementation == PandasDataset:
                 return PandasDataset(df)
             else:
-                return DaskDataset(dd.from_pandas(df), npartitions=4)
+                return DaskDataset(
+                    dd.from_pandas(df, npartitions=4), length=len(df.index)
+                )
         except jsonschema.exceptions.ValidationError:
             return PandasDataset(pd.DataFrame())
 
-    def to_parquet(self, file_path: str) -> str:
+    def to_parquet(self, file_path: str) -> (int, str):
         temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".parquet")
-        dataset = self.from_file(file_path)
-        dataset.data.to_parquet(temp_file.name)
+        df = self._raw_dataset_from_file(file_path)
+        df.to_parquet(temp_file.name)
+        return len(df.index), temp_file.name
 
     def read(self, data):
         pass
