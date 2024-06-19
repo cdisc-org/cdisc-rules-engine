bb97fe131f441d28ecc6b253724a6ca696fbdc03
Sam Johnson <sjohnson@cdisc.org>
2024-06-19 10:37:40 -0400

    added read_metadata test with length estimator

--- tests/unit/test_datasetxpt_metadata_reader.py
+++ tests/unit/test_datasetxpt_metadata_reader.py
@@ -48,6 +48,54 @@ def test_read_metadata():
     assert isinstance(metadata["variable_name_to_size_map"], dict)
 
 
+def test_read_metadata_with_estimate():
+    """
+    Unit test for metadata read using estimate length for xpt file.
+    """
+    test_dataset_path: str = (
+        f"{os.path.dirname(__file__)}/../resources/test_dataset.xpt"
+    )
+    with patch.dict(os.environ, {"DATASET_SIZE_THRESHOLD": "1"}):
+        reader = DatasetXPTMetadataReader(
+            test_dataset_path, file_name="test_dataset.xpt"
+        )
+        assert reader._estimate_dataset_length is True
+        assert reader.row_limit == 1
+
+        metadata: dict = reader.read()
+        assert (
+            metadata["dataset_name"] == "TEST_DATASET"
+        ), "Wrong dataset name. Test file has been changed"
+        assert (
+            metadata["domain_name"] == "EX"
+        ), "Wrong domain name. Test file has been changed"
+        assert (
+            metadata["dataset_label"] == "Exposure"
+        ), "Wrong dataset label. Test file has been changed"
+        assert (
+            metadata["number_of_variables"] == 17
+        ), "Wrong number of variables. Test file has been changed"
+        assert (
+            metadata["dataset_length"] == 1583
+        ), "Wrong estimated dataset length. Test file or estimator has been changed"
+        assert (
+            metadata["dataset_modification_date"] == "2020-08-21T09:14:26"
+        ), "Incorrect modification date. Test file has been changed"
+        assert isinstance(metadata["variable_labels"], list)
+        assert isinstance(metadata["variable_names"], list)
+        assert isinstance(metadata["variable_name_to_data_type_map"], dict)
+        assert any(
+            val in ["Char", "Num"]
+            for val in metadata["variable_name_to_data_type_map"].values()
+        )
+        assert any(
+            val not in ["string", "double", "Character", "Numeric"]
+            for val in metadata["variable_name_to_data_type_map"].values()
+        ), "pyreadstat values has not been converted"
+        assert isinstance(metadata["variable_name_to_label_map"], dict)
+        assert isinstance(metadata["variable_name_to_size_map"], dict)
+
+
 def test_read_metadata_with_variable_formats():
     """
     Unit test for function read.
