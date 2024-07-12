7f6555ee54dd82302e62aff509ce517037ef1a87
Samuel Johnson <sfjohnson24@gmail.com>
2024-07-12 09:54:52 -0400

    update defineXML metadata reader

--- cdisc_rules_engine/services/define_xml/define_xml_reader_2_0.py
+++ cdisc_rules_engine/services/define_xml/define_xml_reader_2_0.py
@@ -31,6 +31,7 @@ class DefineXMLReader20(BaseDefineXMLReader):
             "define_dataset_name": metadata.Name,
             "define_dataset_label": str(metadata.Description.TranslatedText[0]),
             "define_dataset_location": getattr(metadata.leaf, "href", None),
+            "define_dataset_domain": metadata.Domain,
             "define_dataset_class": metadata.Class,
             "define_dataset_structure": str(metadata.Structure),
             # v2.0 does not support is_non_standard. Default to blank
