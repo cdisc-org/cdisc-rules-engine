7a01c030926eceb30530a0dce4c2cdeca8fe2ce2
nhaydel <nicholas.haydel1@gmail.com>
2024-10-28 20:30:21 -0500

    update operations to utilize ExternalDictionariesContainer

--- cdisc_rules_engine/operations/valid_external_dictionary_value.py
+++ cdisc_rules_engine/operations/valid_external_dictionary_value.py
@@ -1,30 +1,19 @@
-from cdisc_rules_engine.exceptions.custom_exceptions import UnsupportedDictionaryType
 from cdisc_rules_engine.operations.base_operation import BaseOperation
-from cdisc_rules_engine.models.dictionaries.dictionary_types import DictionaryTypes
-from cdisc_rules_engine.models.dictionaries.constants import DICTIONARY_VALIDATORS
 
 
 class ValidExternalDictionaryValue(BaseOperation):
     def _execute_operation(self):
-        if self.params.external_dictionary_type not in DictionaryTypes.values():
-            raise UnsupportedDictionaryType(
-                f"{self.params.external_dictionary_type} is not supported by the engine"
+        validator_type = (
+            self.params.external_dictionaries.get_dictionary_validator_class(
+                self.params.external_dictionary_type
             )
-
-        validator_type = DICTIONARY_VALIDATORS.get(self.params.external_dictionary_type)
-        if not validator_type:
-            raise UnsupportedDictionaryType(
-                f"{self.params.external_dictionary_type} is not supported by the "
-                + "valid_external_dictionary_value operation"
-            )
-
+        )
         validator = validator_type(
             cache_service=self.cache,
             data_service=self.data_service,
-            meddra_path=self.params.meddra_path,
-            whodrug_path=self.params.whodrug_path,
-            loinc_path=self.params.loinc_path,
-            medrt_path=self.params.medrt_path,
+            dictionary_path=self.params.external_dictionaries.get_dictionary_path(
+                self.params.external_dictionary_type
+            ),
         )
 
         return self.params.dataframe.apply(
