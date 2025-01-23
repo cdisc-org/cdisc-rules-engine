3e00a8dbefaa38305ed8bf9298163546649a6b13
Gerry Campion <gcampion@cdisc.org>
2025-01-23 00:10:36 -0500

    another try

--- tests/QARegressionTests/test_core/test_utils.py
+++ tests/QARegressionTests/test_core/test_utils.py
@@ -3,18 +3,18 @@ import subprocess
 
 def run_command(args):
     try:
-        process = subprocess.run(
+        completed_process = subprocess.run(
             args,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             universal_newlines=True,
             check=True,
-            shell=True,
+            # shell=True,
         )
         return (
-            process.returncode,
-            process.stdout.lower(),
-            process.stderr.lower(),
+            completed_process.returncode,
+            completed_process.stdout.lower(),
+            completed_process.stderr.lower(),
         )
     except subprocess.CalledProcessError as e:
         return e.returncode, e.stdout.lower(), e.stderr.lower()
