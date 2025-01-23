221e3c5d8f25c8586ed767724d70f13b84aa5879
Gerry Campion <gcampion@cdisc.org>
2025-01-22 23:42:21 -0500

    try subprocess.run for reg tests

--- tests/QARegressionTests/test_core/test_utils.py
+++ tests/QARegressionTests/test_core/test_utils.py
@@ -1,14 +1,20 @@
 import subprocess
 
 
-def run_command(command):
-    process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        text=True,
-    )
-    stdout, stderr = process.communicate()
-    exit_code = process.returncode
-    return exit_code, stdout.lower(), stderr.lower()
+def run_command(args):
+    try:
+        process = subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+            check=True,
+            shell=True,
+        )
+        return (
+            process.returncode,
+            process.stdout.lower(),
+            process.stderr.lower(),
+        )
+    except subprocess.CalledProcessError as e:
+        return e.returncode, e.stdout.lower(), e.stderr.lower()
