50f75c2f179f306f3ecd4934a5e3d1c79ff90995
Gerry Campion <gcampion@cdisc.org>
2025-03-22 15:32:53 -0400

    Fix test

--- tests/unit/test_rules_engine.py
+++ tests/unit/test_rules_engine.py
@@ -1560,7 +1560,7 @@ def test_validate_value_level_metadata_against_define_xml(
             [
                 {
                     "domain": "AE",
-                    "dataset": "ae.xpt",
+                    "dataset": "ae_2.xpt",
                     "executionStatus": ExecutionStatus.SKIPPED.value,
                     "variables": [],
                     "message": "Rule skipped - doesn't apply to domain for rule id=MockRule, dataset=",
