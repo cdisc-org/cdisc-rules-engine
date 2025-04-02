cdisc-org/cdisc-rules-engine

commit d1bc41ca44e0450a4f591865ed9f8d59769dea4d

update error logic (#1113)
* update error logic

* tests

cdisc_rules_engine/models/actions.py
@@ -206,6 +206,11 @@ class COREActions(BaseActions):
         )
         source_row_number: Optional[pd.Series] = data.get(SOURCE_ROW_NUMBER)
         source_filename: Optional[pd.Series] = data.get(SOURCE_FILENAME)
+        row_dict = df_row.to_dict()
+        filtered_dict = {
+            key: ("null" if pd.isna(value) else value)
+            for key, value in row_dict.items()
+        }
         error_object = ValidationErrorEntity(
             dataset=(
                 path.basename(source_filename[df_row.name])
@@ -217,7 +222,7 @@ class COREActions(BaseActions):
                 if isinstance(source_row_number, pd.Series)
                 else (int(df_row.name) + 1)
             ),  # record number should start at 1, not 0
-            value=dict(df_row.to_dict()),
+            value=filtered_dict,
             usubjid=(
                 str(usubjid[df_row.name]) if isinstance(usubjid, pd.Series) else None
             ),

tests/unit/test_rules_engine.py
@@ -2036,13 +2036,13 @@ def test_validate_additional_columns(
                 "message": "Inconsistencies found in enumerated TSVAL columns.",
                 "errors": [
                     {
-                        "value": {"TSVAL": None},
+                        "value": {"TSVAL": "null"},
                         "dataset": "ts.xpt",
                         "row": 2,
                         "USUBJID": "1",
                     },
                     {
-                        "value": {"TSVAL": None},
+                        "value": {"TSVAL": "null"},
                         "dataset": "ts.xpt",
                         "row": 4,
                         "USUBJID": "1",
@@ -2153,17 +2153,17 @@ def test_validate_dataset_contents_against_define_and_library_variable_metadata(
                 {
                     "dataset": "filename",
                     "row": 1,
-                    "value": {"AESEV": None, "AESER": "1"},
+                    "value": {"AESEV": "null", "AESER": "1"},
                 },
                 {
                     "dataset": "filename",
                     "row": 2,
-                    "value": {"AESEV": None, "AESER": "2"},
+                    "value": {"AESEV": "null", "AESER": "2"},
                 },
                 {
                     "dataset": "filename",
                     "row": 3,
-                    "value": {"AESEV": "test", "AESER": None},
+                    "value": {"AESEV": "test", "AESER": "null"},
                 },
             ],
         }

commit 1ea67ee166c448a8f5d6b5bc8a1a65b5d65fa263

Fixed bug caused by #1113 update

cdisc_rules_engine/models/actions.py
@@ -5,6 +5,7 @@ import pandas as pd
 from business_rules.actions import BaseActions, rule_action
 from business_rules.fields import FIELD_TEXT
 
+from cdisc_rules_engine.constants import NULL_FLAVORS
 from cdisc_rules_engine.constants.metadata_columns import (
     SOURCE_FILENAME,
     SOURCE_ROW_NUMBER,
@@ -280,7 +281,7 @@ class COREActions(BaseActions):
         source_filename: Optional[pd.Series] = data.get(SOURCE_FILENAME)
         row_dict = df_row.to_dict()
         filtered_dict = {
-            key: ("null" if pd.isna(value) else value)
+            key: ("null" if value in NULL_FLAVORS else value)
             for key, value in row_dict.items()
         }
         error_object = ValidationErrorEntity(