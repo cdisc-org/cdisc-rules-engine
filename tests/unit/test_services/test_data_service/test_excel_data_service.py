cdisc-org/cdisc-rules-engine

commit d1bc41ca44e0450a4f591865ed9f8d59769dea4d

update error logic (#1113)
* update error logic

* tests

cdisc_rules_engine/models/actions.py
@@ -206,6 +206,11 @@ class COREActions(BaseActions):
         )
         source_row_number: Optional[pd.Series] = data.get(SOURCE_ROW_NUMBER)
         source_filename: Optional[pd.Series] = data.get(SOURCE_FILENAME)
+        row_dict = df_row.to_dict()
+        filtered_dict = {
+            key: ("null" if pd.isna(value) else value)
+            for key, value in row_dict.items()
+        }
         error_object = ValidationErrorEntity(
             dataset=(
                 path.basename(source_filename[df_row.name])
@@ -217,7 +222,7 @@ class COREActions(BaseActions):
                 if isinstance(source_row_number, pd.Series)
                 else (int(df_row.name) + 1)
             ),  # record number should start at 1, not 0
-            value=dict(df_row.to_dict()),
+            value=filtered_dict,
             usubjid=(
                 str(usubjid[df_row.name]) if isinstance(usubjid, pd.Series) else None
             ),

tests/unit/test_rules_engine.py
@@ -2036,13 +2036,13 @@ def test_validate_additional_columns(
                 "message": "Inconsistencies found in enumerated TSVAL columns.",
                 "errors": [
                     {
-                        "value": {"TSVAL": None},
+                        "value": {"TSVAL": "null"},
                         "dataset": "ts.xpt",
                         "row": 2,
                         "USUBJID": "1",
                     },
                     {
-                        "value": {"TSVAL": None},
+                        "value": {"TSVAL": "null"},
                         "dataset": "ts.xpt",
                         "row": 4,
                         "USUBJID": "1",
@@ -2153,17 +2153,17 @@ def test_validate_dataset_contents_against_define_and_library_variable_metadata(
                 {
                     "dataset": "filename",
                     "row": 1,
-                    "value": {"AESEV": None, "AESER": "1"},
+                    "value": {"AESEV": "null", "AESER": "1"},
                 },
                 {
                     "dataset": "filename",
                     "row": 2,
-                    "value": {"AESEV": None, "AESER": "2"},
+                    "value": {"AESEV": "null", "AESER": "2"},
                 },
                 {
                     "dataset": "filename",
                     "row": 3,
-                    "value": {"AESEV": "test", "AESER": None},
+                    "value": {"AESEV": "test", "AESER": "null"},
                 },
             ],
         }

commit 60ce0f326249830e488a4d8406b118377a57ec87

Direct excel datasets added (#1114)
* Direct excel datasets added

* merge get_dataset_metadata from data services to base class

* Fix contents_define_dataset_builder test

* Fixed bug caused by #1113 update

* Added error message for invalid excel files

* Fix excel datatypes

* Fix excel dataset record count

cdisc_rules_engine/enums/dataformat_types.py
@@ -6,4 +6,5 @@ class DataFormatTypes(BaseEnum):
     NDJSON = "NDJSON"
     PARQUET = "PARQUET"
     USDM = "USDM"
+    XLSX = "XLSX"
     XPT = "XPT"

cdisc_rules_engine/models/actions.py
@@ -5,6 +5,7 @@ import pandas as pd
 from business_rules.actions import BaseActions, rule_action
 from business_rules.fields import FIELD_TEXT
 
+from cdisc_rules_engine.constants import NULL_FLAVORS
 from cdisc_rules_engine.constants.metadata_columns import (
     SOURCE_FILENAME,
     SOURCE_ROW_NUMBER,
@@ -280,7 +281,7 @@ class COREActions(BaseActions):
         source_filename: Optional[pd.Series] = data.get(SOURCE_FILENAME)
         row_dict = df_row.to_dict()
         filtered_dict = {
-            key: ("null" if pd.isna(value) else value)
+            key: ("null" if value in NULL_FLAVORS else value)
             for key, value in row_dict.items()
         }
         error_object = ValidationErrorEntity(

cdisc_rules_engine/services/data_services/__init__.py
@@ -3,6 +3,7 @@ from .local_data_service import LocalDataService
 from .dummy_data_service import DummyDataService
 from .usdm_data_service import USDMDataService
 from .data_service_factory import DataServiceFactory
+from .excel_data_service import ExcelDataService
 
 __all__ = [
     "BaseDataService",
@@ -10,5 +11,6 @@ __all__ = [
     "LocalDataService",
     "DataServiceFactory",
     "DummyDataService",
+    "ExcelDataService",
     "USDMDataService",
 ]

cdisc_rules_engine/services/data_services/base_data_service.py
@@ -177,6 +177,23 @@ class BaseDataService(DataServiceInterface, ABC):
             dataset, dataset_metadata, file_path, datasets
         )
 
+    @cached_dataset(DatasetTypes.METADATA.value)
+    def get_dataset_metadata(
+        self, dataset_name: str, size_unit: str = None, **params
+    ) -> DatasetInterface:
+        """
+        Gets metadata of a dataset and returns it as a DataFrame.
+        """
+        dataset_metadata = self.get_raw_dataset_metadata(dataset_name=dataset_name)
+        metadata_to_return: dict = {
+            "dataset_size": [dataset_metadata.file_size],
+            "dataset_location": [dataset_metadata.filename],
+            "dataset_name": [dataset_metadata.name],
+            "dataset_label": [dataset_metadata.label],
+            "record_count": [dataset_metadata.record_count],
+        }
+        return self.dataset_implementation.from_dict(metadata_to_return)
+
     def _handle_special_cases(
         self,
         dataset: DatasetInterface,

cdisc_rules_engine/services/data_services/data_service_factory.py
@@ -8,6 +8,9 @@ from cdisc_rules_engine.interfaces import (
     FactoryInterface,
 )
 from cdisc_rules_engine.models.dataset import DaskDataset, PandasDataset
+from cdisc_rules_engine.services.data_services.excel_data_service import (
+    ExcelDataService,
+)
 
 
 from . import DummyDataService, LocalDataService, USDMDataService
@@ -22,6 +25,7 @@ class DataServiceFactory(FactoryInterface):
         "local": LocalDataService,
         "dummy": DummyDataService,
         "usdm": USDMDataService,
+        "excel": ExcelDataService,
     }
 
     def __init__(
@@ -67,6 +71,17 @@ class DataServiceFactory(FactoryInterface):
             """Get dummy data service"""
             return self.get_dummy_data_service(
                 data=DummyDataService.get_data(dataset_paths)
+            )
+        elif ExcelDataService.is_valid_data(dataset_paths):
+            """Get Excel file to dataset data service"""
+            return self.get_service(
+                "excel",
+                standard=self.standard,
+                standard_version=self.standard_version,
+                standard_substandard=self.standard_substandard,
+                library_metadata=self.library_metadata,
+                dataset_path=dataset_paths[0],
+                dataset_implementation=self.get_dataset_implementation(),
             )
         else:
             """Get local Directory data service"""

cdisc_rules_engine/services/data_services/dummy_data_service.py
@@ -67,17 +67,13 @@ class DummyDataService(BaseDataService):
         else:
             return PandasDataset.from_dict({})
 
-    def get_dataset_metadata(self, dataset_name: str, **kwargs):
-        dataset_metadata: dict = self.__get_dataset_metadata(dataset_name, **kwargs)
-        return PandasDataset.from_dict(dataset_metadata)
-
     def get_raw_dataset_metadata(
         self, dataset_name: str, **kwargs
     ) -> SDTMDatasetMetadata:
         dataset_metadata: dict = self.__get_dataset_metadata(dataset_name, **kwargs)
         return SDTMDatasetMetadata(
             name=dataset_metadata["dataset_name"][0],
-            domain=dataset_metadata["dataset_name"][0],
+            first_record={"DOMAIN": dataset_metadata["dataset_name"][0]},
             label=dataset_metadata["dataset_label"][0],
             modification_date=datetime.now().isoformat(),
             filename=dataset_metadata["filename"][0],

cdisc_rules_engine/services/data_services/excel_data_service.py
@@ -1,0 +1,204 @@
+import os
+from io import IOBase
+from typing import List, Sequence
+from datetime import datetime
+import re
+import pandas as pd
+from numpy import nan
+
+from cdisc_rules_engine.interfaces import CacheServiceInterface, ConfigInterface
+from cdisc_rules_engine.models.dataset.dataset_interface import DatasetInterface
+from cdisc_rules_engine.models.dataset.pandas_dataset import PandasDataset
+from cdisc_rules_engine.models.sdtm_dataset_metadata import SDTMDatasetMetadata
+from cdisc_rules_engine.models.dataset_types import DatasetTypes
+from cdisc_rules_engine.models.variable_metadata_container import (
+    VariableMetadataContainer,
+)
+from cdisc_rules_engine.services import logger
+from cdisc_rules_engine.services.data_readers.data_reader_factory import (
+    DataReaderFactory,
+)
+from .base_data_service import BaseDataService, cached_dataset
+
+DATASETS_SHEET_NAME = "Datasets"
+DATASET_FILENAME_COLUMN = "Filename"
+DATASET_LABEL_COLUMN = "Label"
+
+
+class ExcelDataService(BaseDataService):
+    _instance = None
+
+    def __init__(
+        self,
+        cache_service: CacheServiceInterface,
+        reader_factory: DataReaderFactory,
+        config: ConfigInterface,
+        **kwargs,
+    ):
+        super(ExcelDataService, self).__init__(
+            cache_service, reader_factory, config, **kwargs
+        )
+        self.dataset_path: str = kwargs.get("dataset_path", "")
+
+    @classmethod
+    def get_instance(
+        cls,
+        cache_service: CacheServiceInterface,
+        config: ConfigInterface = None,
+        **kwargs,
+    ):
+        if cls._instance is None:
+            service = cls(
+                cache_service=cache_service,
+                reader_factory=DataReaderFactory(
+                    dataset_implementation=kwargs.get(
+                        "dataset_implementation", PandasDataset
+                    )
+                ),
+                config=config,
+                **kwargs,
+            )
+            cls._instance = service
+        return cls._instance
+
+    def has_all_files(self, prefix: str, file_names: List[str]) -> bool:
+        return os.path.isfile(self.dataset_path)
+
+    def get_file_matching_pattern(self, prefix: str, pattern: str) -> str:
+        """
+        Returns the path to the file if one matches the pattern given, otherwise
+        return None.
+        """
+        for f in os.listdir(prefix):
+            if os.path.isfile(os.path.join(prefix, f)) and re.match(pattern, f):
+                return f
+        return None
+
+    @cached_dataset(DatasetTypes.CONTENTS.value)
+    def get_dataset(self, dataset_name: str, **params) -> DatasetInterface:
+        dtype_mapping = {
+            "Char": str,
+            "Num": float,
+            "Boolean": bool,
+            "Number": float,
+            "String": str,
+        }
+        header = pd.read_excel(
+            self.dataset_path, sheet_name=dataset_name, header=None, nrows=3
+        )
+        dtypes = dict(zip(header.iloc[0].tolist(), header.iloc[2].tolist()))
+        dtypes = {key: dtype_mapping.get(value, str) for key, value in dtypes.items()}
+        dataframe = pd.read_excel(
+            self.dataset_path,
+            sheet_name=dataset_name,
+            dtype=dtypes,
+            skiprows=(1, 2, 3),
+        )
+        dataframe = dataframe.replace({nan: None})
+        dataset = PandasDataset(dataframe)
+        return dataset
+
+    @cached_dataset(DatasetTypes.RAW_METADATA.value)
+    def get_raw_dataset_metadata(
+        self, dataset_name: str, **kwargs
+    ) -> SDTMDatasetMetadata:
+        """
+        Returns dataset metadata as DatasetMetadata instance.
+        """
+        datasets_worksheet = pd.read_excel(
+            self.dataset_path, sheet_name=DATASETS_SHEET_NAME
+        )
+        metadata = datasets_worksheet[
+            datasets_worksheet[DATASET_FILENAME_COLUMN] == dataset_name
+        ]
+        dataset = self.get_dataset(dataset_name=dataset_name)
+        return SDTMDatasetMetadata(
+            name=dataset_name.split(".")[0].upper(),
+            first_record=(dataset.data.iloc[0].to_dict() if not dataset.empty else {}),
+            label=metadata[DATASET_LABEL_COLUMN].iloc[0] if not metadata.empty else "",
+            modification_date=datetime.fromtimestamp(
+                os.path.getmtime(self.dataset_path)
+            ).isoformat(),
+            filename=dataset_name,
+            full_path=dataset_name,
+            file_size=0,
+            record_count=len(dataset),
+        )
+
+    @cached_dataset(DatasetTypes.VARIABLES_METADATA.value)
+    def get_variables_metadata(self, dataset_name: str, **params) -> DatasetInterface:
+        """
+        Gets dataset from blob storage and returns metadata of a certain variable.
+        """
+        dataframe = pd.read_excel(
+            self.dataset_path, sheet_name=dataset_name, header=None, nrows=4
+        )
+        metadata_to_return: VariableMetadataContainer = VariableMetadataContainer(
+            {
+                "variable_names": dataframe.iloc[0].tolist(),
+                "variable_labels": dataframe.iloc[1].tolist(),
+                "variable_formats": [""] * dataframe.shape[1],
+                "variable_name_to_label_map": dict(
+                    zip(dataframe.iloc[0].tolist(), dataframe.iloc[1].tolist())
+                ),
+                "variable_name_to_data_type_map": dict(
+                    zip(dataframe.iloc[0].tolist(), dataframe.iloc[2].tolist())
+                ),
+                "variable_name_to_size_map": dict(
+                    zip(
+                        dataframe.iloc[0].tolist(),
+                        dataframe.iloc[3].tolist(),
+                    )
+                ),
+                "number_of_variables": dataframe.shape[1],
+            }
+        )
+        return self._reader_factory.dataset_implementation.from_dict(
+            metadata_to_return.to_representation()
+        )
+
+    @cached_dataset(DatasetTypes.CONTENTS.value)
+    def get_define_xml_contents(self, dataset_name: str) -> bytes:
+        """
+        Reads local define xml file as bytes
+        """
+        with open(dataset_name, "rb") as f:
+            return f.read()
+
+    def read_data(self, file_path: str) -> IOBase:
+        return open(file_path, "rb")
+
+    def get_datasets(self) -> List[dict]:
+        try:
+            worksheet = pd.read_excel(self.dataset_path, sheet_name=DATASETS_SHEET_NAME)
+        except TypeError as e:
+            logger.error(
+                f"Failed to read datasets from the Excel file at {self.dataset_path}. "
+                f"Ensure the file is in the correct format. "
+                f"Try opening and saving the file in Microsoft Excel. "
+                f"Error: {str(e)}"
+            )
+            raise
+        datasets = [
+            self.get_raw_dataset_metadata(dataset_name=dataset_filename)
+            for dataset_filename in worksheet[DATASET_FILENAME_COLUMN]
+        ]
+        return datasets
+
+    def to_parquet(self, file_path: str) -> str:
+        """
+        Stub implementation to satisfy abstract interface requirements.
+
+        This method exists only to fulfill the abstract method requirement from the parent class.
+        While implemented to prevent TypeError, it is not intended to be called in this class.
+        Other classes implementing this interface make actual use of to_parquet().
+        """
+        raise NotImplementedError("to_parquet is not supported for this class")
+
+    @staticmethod
+    def is_valid_data(dataset_paths: Sequence[str]):
+        return (
+            dataset_paths
+            and len(dataset_paths) == 1
+            and dataset_paths[0].lower().endswith(".xlsx")
+        )

cdisc_rules_engine/services/data_services/local_data_service.py
@@ -93,24 +93,6 @@ class LocalDataService(BaseDataService):
         df = reader.from_file(dataset_name)
         return df
 
-    @cached_dataset(DatasetTypes.METADATA.value)
-    def get_dataset_metadata(
-        self, dataset_name: str, size_unit: str = None, **params
-    ) -> DatasetInterface:
-        """
-        Gets metadata of a dataset and returns it as a DataFrame.
-        """
-        file_metadata, contents_metadata = self.__get_dataset_metadata(
-            dataset_name, size_unit=size_unit, **params
-        )
-        metadata_to_return: dict = {
-            "dataset_size": [file_metadata["file_size"]],
-            "dataset_location": [file_metadata["name"]],
-            "dataset_name": [contents_metadata["dataset_name"]],
-            "dataset_label": [contents_metadata["dataset_label"]],
-        }
-        return self.dataset_implementation.from_dict(metadata_to_return)
-
     @cached_dataset(DatasetTypes.RAW_METADATA.value)
     def get_raw_dataset_metadata(
         self, dataset_name: str, **kwargs

cdisc_rules_engine/services/data_services/usdm_data_service.py
@@ -90,18 +90,6 @@ class USDMDataService(BaseDataService):
     def get_dataset(self, dataset_name: str, **params) -> DatasetInterface:
         return self.__get_dataset(dataset_name)
 
-    @cached_dataset(DatasetTypes.METADATA.value)
-    def get_dataset_metadata(
-        self, dataset_name: str, size_unit: str = None, **params
-    ) -> DatasetInterface:
-        """
-        Gets metadata of a dataset and returns it as a DataFrame.
-        """
-        metadata_to_return: dict = {
-            "dataset_name": [dataset_name],
-        }
-        return self._reader_factory.dataset_implementation.from_dict(metadata_to_return)
-
     @cached_dataset(DatasetTypes.RAW_METADATA.value)
     def get_raw_dataset_metadata(
         self, dataset_name: str, **kwargs

tests/unit/test_dataset_builders/test_content_metadata_dataset_builder.py
@@ -281,9 +281,9 @@ def test_ContentMetadataDatasetBuilder_split_datasets(conditions):
     datasets = [DummyDataset(data) for data in data_metadata.get("datasets", [])]
     expected_output = {
         "dataset_size": 1000,
+        "dataset_location": "qscg.xpt",
         "dataset_name": "QSCG",
         "dataset_label": "Clinical Global Impressions",
-        "filename": "qscg.xpt",
         "record_count": 1,
     }
     expected = PandasDataset(
@@ -308,9 +308,9 @@ def test_ContentMetadataDatasetBuilder_split_datasets(conditions):
     ).build()
     expected_output2 = {
         "dataset_size": 1000,
+        "dataset_location": "qspg.xpt",
         "dataset_name": "QSPG",
         "dataset_label": "Patient Global Impressions",
-        "filename": "qspg.xpt",
         "record_count": 1,
     }
     expected2 = PandasDataset(

tests/unit/test_dataset_builders/test_contents_define_dataset_builder.py
@@ -479,7 +479,7 @@ expected_results = {
             "Exposure as Collected AA",
             "Exposure as Collected AA",
         ],
-        "filename": [
+        "dataset_location": [
             "ecaa.xpt",
             "ecaa.xpt",
             "ecaa.xpt",
@@ -562,7 +562,7 @@ expected_results = {
             "Exposure as Collected BB",
             "Exposure as Collected BB",
         ],
-        "filename": ["ecbb.xpt", "ecbb.xpt", "ecbb.xpt"],
+        "dataset_location": ["ecbb.xpt", "ecbb.xpt", "ecbb.xpt"],
         "record_count": [3, 3, 3],
         "define_dataset_name": ["ECBB", "ECBB", "ECBB"],
         "define_dataset_label": ["ECBB label", "ECBB label", "ECBB label"],
@@ -721,7 +721,7 @@ expected_results = {
             "Supplemental Qualifiers for EC",
             "Supplemental Qualifiers for EC",
         ],
-        "filename": [
+        "dataset_location": [
             "suppec.xpt",
             "suppec.xpt",
             "suppec.xpt",
@@ -814,12 +814,14 @@ expected_results = {
     ],
 )
 def test_contents_define_dataset_builder(dataset_path):
+    cache_mock = MagicMock()
+    cache_mock.get_dataset.return_value = None
     builder = ContentsDefineDatasetBuilder(
         rule={
             "conditions": ConditionCompositeFactory.get_condition_composite({}),
         },
         data_service=DummyDataService(
-            MagicMock(),
+            cache_mock,
             MagicMock(),
             MagicMock(),
             data=[DummyDataset(dataset) for dataset in datasets],

tests/unit/test_services/test_data_service/test_data_service.py
@@ -49,6 +49,9 @@ def test_get_dataset_metadata(mock_read_metadata: MagicMock, dataset_metadata: d
                 "dataset_label": [
                     dataset_metadata["contents_metadata"]["dataset_label"]
                 ],
+                "record_count": [
+                    dataset_metadata["contents_metadata"]["dataset_length"]
+                ],
             }
         )
     )
@@ -179,7 +182,7 @@ def test_get_dataset_class(dataset_metadata, data, expected_class):
     mock_cache_service = MagicMock()
     library_metadata: LibraryMetadataContainer = get_library_metadata_from_cache(
         Validation_args(
-            f"{os.path.dirname(__file__)}/../../resources/cache",
+            f"{os.path.dirname(__file__)}/../../../../resources/cache",
             10,
             [],
             "",
@@ -257,7 +260,7 @@ def test_get_dataset_class_associated_domains():
     ):
         library_metadata: LibraryMetadataContainer = get_library_metadata_from_cache(
             Validation_args(
-                f"{os.path.dirname(__file__)}/../../resources/cache",
+                f"{os.path.dirname(__file__)}/../../../../resources/cache",
                 10,
                 [],
                 "",

tests/unit/test_services/test_data_service/test_dummy_data_service.py
@@ -99,13 +99,13 @@ def test_get_dataset_metadata():
         }
     ]
     datasets = [DummyDataset(dataset) for dataset in dataset_data]
-    data_service = DummyDataService(
-        MagicMock(), MagicMock(), MagicMock(), data=datasets
-    )
-    metadata = data_service.get_dataset_metadata("ae.xpt")
-    assert metadata["dataset_label"].iloc[0] == "ADVERSE EVENTS"
-    assert metadata["dataset_name"].iloc[0] == "AE"
-    assert metadata["dataset_size"].iloc[0] == 2000
+    cache_mock = MagicMock()
+    cache_mock.get_dataset.return_value = None
+    data_service = DummyDataService(cache_mock, MagicMock(), MagicMock(), data=datasets)
+    metadata = data_service.get_dataset_metadata(dataset_name="ae.xpt")
+    assert metadata["dataset_label"][0] == "ADVERSE EVENTS"
+    assert metadata["dataset_name"][0] == "AE"
+    assert metadata["dataset_size"][0] == 2000
 
 
 def test_get_variables_metadata():

tests/unit/test_services/test_data_service/test_excel_data_service.py
@@ -1,0 +1,96 @@
+import os
+from unittest.mock import MagicMock
+
+import pytest
+from cdisc_rules_engine.config.config import ConfigService
+
+from cdisc_rules_engine.services.data_services import ExcelDataService
+from cdisc_rules_engine.models.dataset import PandasDataset
+
+
+@pytest.mark.parametrize(
+    "dataset_name",
+    ("ecaa.xpt", "ecbb.xpt", "suppec.xpt"),
+)
+def test_get_dataset(dataset_name):
+    dataset_path = f"{os.path.dirname(__file__)}/../../../resources/test_datasets.xlsx"
+    mock_cache = MagicMock()
+    mock_cache.get_dataset.return_value = None
+    data_service = ExcelDataService.get_instance(
+        config=ConfigService(),
+        cache_service=mock_cache,
+        dataset_implementation=PandasDataset,
+        dataset_path=dataset_path,
+    )
+    data = data_service.get_dataset(dataset_name=dataset_name)
+    assert isinstance(data, PandasDataset)
+
+
+@pytest.mark.parametrize(
+    "expected_result",
+    (
+        {
+            "dataset_location": "ecaa.xpt",
+            "dataset_label": "Exposure as Collected AA",
+            "dataset_name": "ECAA",
+            "dataset_size": 0,
+            "record_count": 8,
+        },
+        {
+            "dataset_location": "ecbb.xpt",
+            "dataset_label": "Exposure as Collected BB",
+            "dataset_name": "ECBB",
+            "dataset_size": 0,
+            "record_count": 3,
+        },
+        {
+            "dataset_location": "suppec.xpt",
+            "dataset_label": "Supplemental Qualifiers for EC",
+            "dataset_name": "SUPPEC",
+            "dataset_size": 0,
+            "record_count": 11,
+        },
+    ),
+)
+def test_get_dataset_metadata(expected_result):
+    dataset_path = f"{os.path.dirname(__file__)}/../../../resources/test_datasets.xlsx"
+    cache_mock = MagicMock()
+    cache_mock.get_dataset.return_value = None
+    data_service = ExcelDataService(
+        cache_mock, MagicMock(), MagicMock(), dataset_path=dataset_path
+    )
+    metadata = data_service.get_dataset_metadata(
+        dataset_name=expected_result["dataset_location"]
+    )
+    assert metadata["dataset_label"][0] == expected_result["dataset_label"]
+    assert metadata["dataset_name"][0] == expected_result["dataset_name"]
+    assert metadata["dataset_size"][0] == expected_result["dataset_size"]
+    assert metadata["dataset_location"][0] == expected_result["dataset_location"]
+    assert metadata["record_count"][0] == expected_result["record_count"]
+
+
+@pytest.mark.parametrize(
+    "dataset_name",
+    ("ecaa.xpt", "ecbb.xpt", "suppec.xpt"),
+)
+def test_get_variables_metdata(dataset_name):
+    dataset_path = f"{os.path.dirname(__file__)}/../../../resources/test_datasets.xlsx"
+    mock_cache = MagicMock()
+    mock_cache.get_dataset.return_value = None
+    data_service = ExcelDataService.get_instance(
+        config=ConfigService(),
+        cache_service=mock_cache,
+        dataset_implementation=PandasDataset,
+        dataset_path=dataset_path,
+    )
+    data = data_service.get_variables_metadata(dataset_name=dataset_name, datasets=[])
+    assert isinstance(data, PandasDataset)
+    expected_keys = [
+        "variable_name",
+        "variable_format",
+        "variable_order_number",
+        "variable_data_type",
+        "variable_label",
+    ]
+    for key in expected_keys:
+        assert key in data

tests/unit/test_services/test_data_service/test_local_data_service.py
@@ -12,7 +12,7 @@ def test_read_metadata():
     """
     Unit test for read_data method.
     """
-    dataset_path = f"{os.path.dirname(__file__)}/../resources/test_dataset.xpt"
+    dataset_path = f"{os.path.dirname(__file__)}/../../../resources/test_dataset.xpt"
     data_service = LocalDataService(MagicMock(), MagicMock(), MagicMock())
     metadata = data_service.read_metadata(dataset_path)
     assert "file_metadata" in metadata
@@ -37,7 +37,7 @@ def test_read_metadata():
     ],
 )
 def test_has_all_files(files, expected_result):
-    directory = f"{os.path.dirname(__file__)}/../resources"
+    directory = f"{os.path.dirname(__file__)}/../../../resources"
     data_service = LocalDataService(MagicMock(), MagicMock(), MagicMock())
     assert data_service.has_all_files(directory, files) == expected_result
 
@@ -47,7 +47,7 @@ def test_has_all_files(files, expected_result):
     [PandasDataset],
 )
 def test_get_dataset(dataset_implementation):
-    dataset_path = f"{os.path.dirname(__file__)}/../resources/test_dataset.xpt"
+    dataset_path = f"{os.path.dirname(__file__)}/../../../resources/test_dataset.xpt"
     mock_cache = MagicMock()
     mock_cache.get_dataset.return_value = None
     data_service = LocalDataService.get_instance(
@@ -64,7 +64,9 @@ def test_get_dataset(dataset_implementation):
     [PandasDataset],
 )
 def test_get_variables_metdata(dataset_implementation):
-    dataset_path = f"{os.path.dirname(__file__)}/../resources/test_adam_dataset.xpt"
+    dataset_path = (
+        f"{os.path.dirname(__file__)}/../../../resources/test_adam_dataset.xpt"
+    )
     mock_cache = MagicMock()
     mock_cache.get_dataset.return_value = None
     data_service = LocalDataService.get_instance(