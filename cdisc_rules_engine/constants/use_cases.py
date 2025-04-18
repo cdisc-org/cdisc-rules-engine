dc6681f13f6f2708cd10733d5a41476707857cac
Samuel Johnson <sfjohnson24@gmail.com>
2025-04-18 17:01:19 -0400

    update mode

--- cdisc_rules_engine/constants/use_cases.py
+++ cdisc_rules_engine/constants/use_cases.py
@@ -19,51 +19,26 @@ USE_CASE_DOMAINS = {
     SDTM: {
         INDH: [],
         PROD: [],
-        NONCLIN: [],  # Empty as NONCLIN may not apply to SDTM
-        ANALYSIS: [],  # Empty as ANALYSIS may not apply to SDTM
+        NONCLIN: [],
+        ANALYSIS: [],
     },
     SEND: {
-        INDH: [],  # Empty as INDH may not apply to SEND
-        PROD: [],  # Empty as PROD may not apply to SEND
-        NONCLIN: [
-            "BG",
-            "BL",
-            "BW",
-            "CL",
-            "DD",
-            "DM",
-            "EX",
-            "FW",
-            "LB",
-            "TS",
-            "PC",
-            "PP",
-            "TX",
-            "VS",
-        ],
-        ANALYSIS: [],  # Empty as ANALYSIS may not apply to SEND
+        INDH: [],
+        PROD: [],
+        NONCLIN: [],
+        ANALYSIS: [],
     },
     ADAM: {
-        INDH: [],  # Empty as INDH may not have specific ADaM domains
-        PROD: [],  # Empty as PROD may not have specific ADaM domains
-        NONCLIN: [],  # Empty as NONCLIN may not have specific ADaM domains
-        ANALYSIS: [
-            "ADAE",
-            "ADSL",
-            "ADVS",
-            "ADLB",
-            "ADEG",
-            "ADQSCISR",
-            "ADPP",
-            "ADPC",
-            "ADTTE",
-        ],
+        INDH: [],
+        PROD: [],
+        NONCLIN: [],
+        ANALYSIS: [],
     },
-    # no conformance ruels for CDASH Presently
+    # no conformance rules for CDASH Presently
     CDASH: {
         INDH: [],
         PROD: [],
         NONCLIN: [],
-        ANALYSIS: [],  # Empty as ANALYSIS may not apply to CDASH
+        ANALYSIS: [],
     },
 }
