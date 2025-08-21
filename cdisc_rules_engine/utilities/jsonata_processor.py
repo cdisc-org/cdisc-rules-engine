7a401b4d0a84299c88dab6b3389f873dfb3a929f
Gerry Campion <gcampion@cdisc.org>
2025-08-21 10:13:29 -0400

    reporting works

--- cdisc_rules_engine/utilities/jsonata_processor.py
+++ cdisc_rules_engine/utilities/jsonata_processor.py
@@ -1,5 +1,12 @@
 from jsonata import Jsonata
 
+from cdisc_rules_engine.enums.execution_status import ExecutionStatus
+from cdisc_rules_engine.models.validation_error_container import (
+    ValidationErrorContainer,
+)
+from cdisc_rules_engine.models.validation_error_entity import (
+    ValidationErrorEntity,
+)
 from cdisc_rules_engine.models.rule_conditions.condition_composite import (
     ConditionComposite,
 )
@@ -14,5 +21,31 @@ class JSONataProcessor:
         condition: SingleCondition = conditions.get_conditions().get("all", [])[0]
         check = condition.get_conditions().get("operator")
         expr = Jsonata(check)
-        result = expr.evaluate(dataset)
-        return result
+        results = expr.evaluate(dataset)
+        errors = (
+            [
+                ValidationErrorEntity(
+                    value=result,
+                    dataset=dataset_metadata.name,
+                    row=result.get("path"),
+                    usubjid=result.get("id"),
+                    sequence=result.get("iid"),
+                )
+                for result in results
+            ]
+            if results
+            else []
+        )
+        validation_error_container = ValidationErrorContainer(
+            dataset=dataset_metadata.name,
+            domain=dataset_metadata.domain,
+            targets=rule.get("output_variables"),
+            errors=errors,
+            message=rule.get("message"),
+            status=(
+                ExecutionStatus.SUCCESS.value
+                if results
+                else ExecutionStatus.EXECUTION_ERROR.value
+            ),
+        )
+        return [validation_error_container.to_representation()]
