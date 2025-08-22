3eef17c743b4b9cfa2a4f409e6ebad229140615d
Gerry Campion <gcampion@cdisc.org>
2025-08-22 16:39:03 -0400

    jsonata can now be direct value of the Check property

--- cdisc_rules_engine/utilities/jsonata_processor.py
+++ cdisc_rules_engine/utilities/jsonata_processor.py
@@ -7,19 +7,13 @@ from cdisc_rules_engine.models.validation_error_container import (
 from cdisc_rules_engine.models.validation_error_entity import (
     ValidationErrorEntity,
 )
-from cdisc_rules_engine.models.rule_conditions.condition_composite import (
-    ConditionComposite,
-)
-from cdisc_rules_engine.models.rule_conditions.single_condition import SingleCondition
 
 
 class JSONataProcessor:
 
     @staticmethod
     def execute_jsonata_rule(rule, dataset, datasets, dataset_metadata, **kwargs):
-        conditions: ConditionComposite = rule.get("conditions")
-        condition: SingleCondition = conditions.get_conditions().get("all", [])[0]
-        check = condition.get_conditions().get("operator")
+        check = rule.get("conditions")
         expr = Jsonata(check)
         results = expr.evaluate(dataset)
         errors = (
