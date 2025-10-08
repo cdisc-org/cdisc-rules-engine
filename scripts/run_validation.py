e06d7a28fa1aa3a70816799a0a16bd5bc52ee80c
Samuel Johnson <sfjohnson24@gmail.com>
2025-10-08 15:03:59 -0400

    kwarg

--- scripts/run_validation.py
+++ scripts/run_validation.py
@@ -266,6 +266,7 @@ def run_single_rule_validation(
         standard_version=standard_version,
         standard_substandard=standard_substandard,
         library_metadata=library_metadata,
+        max_errors_per_rule=None,
     )
     engine.rule_processor = RuleProcessor(data_service, cache, library_metadata)
     engine.data_processor = DataProcessor(data_service, cache)
