a175ebef5a83ff4ddf963f7b93a08dee752ddcd5
alexfurmenkov <alexeyfurmenkov@gmail.com>
2025-10-09 15:53:30 +0200

    add dataset

--- cdisc_rules_engine/dataset_builders/json_schema_check_dataset_builder.py
+++ cdisc_rules_engine/dataset_builders/json_schema_check_dataset_builder.py
@@ -1,11 +1,10 @@
 from cdisc_rules_engine.dataset_builders.values_dataset_builder import (
     ValuesDatasetBuilder,
 )
+from cdisc_rules_engine.models.dataset import PandasDataset
 
 
 class JsonSchemaCheckDatasetBuilder(ValuesDatasetBuilder):
     def get_dataset(self):
-        dataset_json = {
-            "json": self.data_service.json,
-        }
-        return dataset_json
+        dataset_dict = {"json": [self.data_service.json]}
+        return PandasDataset.from_dict(dataset_dict)
