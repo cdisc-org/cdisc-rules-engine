68407267be8d79ed8d6d06e8098f8db790b33629
alexfurmenkov <alexeyfurmenkov@gmail.com>
2025-11-12 18:53:43 +0100

    add unit test for split_by operation

--- cdisc_rules_engine/operations/split_by.py
+++ cdisc_rules_engine/operations/split_by.py
@@ -3,6 +3,6 @@ from cdisc_rules_engine.operations.base_operation import BaseOperation
 
 class SplitBy(BaseOperation):
     def _execute_operation(self):
-        return self.params.dataframe[self.params.target].str.split(
+        return self.evaluation_dataset[self.params.target].str.split(
             self.params.delimiter
         )
