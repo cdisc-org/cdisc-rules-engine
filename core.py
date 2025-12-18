0fecbd060d17da75dddd5170834ba2d7aeee3451
Pradeep Reddy Ramana <pradeepreddy_ramana@eisai.com>
2025-12-18 10:36:13 -0500

    more robust  type declaration

--- core.py
+++ core.py
@@ -12,6 +12,7 @@ import pickle
 import tempfile
 from datetime import datetime
 from multiprocessing import freeze_support
+from typing import Union
 from dotenv import load_dotenv
 
 import click
@@ -380,7 +381,7 @@ def validate(
     progress: str,
     define_xml_path: str,
     validate_xml: str,
-    jsonata_custom_functions: tuple[()] | tuple[tuple[str, str], ...],
+    jsonata_custom_functions: Union[tuple[()], tuple[tuple[str, str], ...]],
     max_report_rows: int,
     max_errors_per_rule: tuple[int, bool],
 ):
