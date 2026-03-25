ab9614a70aa9c453e2a0887e2a351b682cbe5449
alexfurmenkov <alexeyfurmenkov@gmail.com>
2026-03-25 19:29:33 +0100

    #1652 wildcard replacement with domain on grouping_variables

--- cdisc_rules_engine/models/actions.py
+++ cdisc_rules_engine/models/actions.py
@@ -191,6 +191,10 @@ class COREActions(BaseActions):
                     targets,
                 )
 
+            grouping_variables = [
+                x.replace("--", self.dataset_metadata.domain_cleaned or "")
+                for x in grouping_variables
+            ]
             missing_grouping_vars = [
                 var for var in grouping_variables if var not in data.columns
             ]
