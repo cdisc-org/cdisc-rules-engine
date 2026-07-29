9f2f8494abf3f0f45a8c08e7e618a3093b0bedce
Niemes, Adam <A.Niemes@Medpace.com>
2026-07-29 08:54:00 -0400

    Fixed merge conflicts

--- tests/QARegressionTests/test_Issues/test_CoreIssue1421.py
+++ tests/QARegressionTests/test_Issues/test_CoreIssue1421.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import openpyxl
+import pytest
 from conftest import get_python_executable
 from QARegressionTests.globals import (
     dataset_details_sheet,
@@ -10,6 +11,7 @@ from QARegressionTests.globals import (
 )
 
 
+@pytest.mark.regression
 def test_validate_define_xml_against_lib_metadata():
     """Validates that codelist definitions in a Define-XML are checked
     against CDISC library CT metadata via rule CDISC.SDTMIG.CG0999: verifies
